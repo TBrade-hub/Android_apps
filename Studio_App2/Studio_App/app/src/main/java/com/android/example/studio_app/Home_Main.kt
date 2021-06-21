69f641461cc8ab2ce91d07f8e68f25dfe3a84ab9
samue <samuelebrandenburg12@gmail.com>
2021-06-21 16:59:34 -0500

    Set up logout feature

--- Studio_App2/Studio_App/app/src/main/java/com/android/example/studio_app/Home_Main.kt
+++ Studio_App2/Studio_App/app/src/main/java/com/android/example/studio_app/Home_Main.kt
@@ -1,24 +1,73 @@
 package com.android.example.studio_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.viewpager.widget.ViewPager
 import com.android.example.studio_app.pageAdapter.HomePageFragmentAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Home_Main : AppCompatActivity() {
     // viewPager
-    private lateinit var tabViewAdapter: HomePageFragmentAdapter
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabLayout: TabLayout
 
+
+
+
+    private companion object {
+        private lateinit var tabViewAdapter: HomePageFragmentAdapter
+        private lateinit var viewPager: ViewPager
+        private lateinit var tabLayout: TabLayout
+        private const val TAG = "Home_main"
+    }
+
+
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_main)
+        auth = Firebase.auth
+
+
         setUpViewPager()
 
     }
+
+
+
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //Inflates the menu
+        menuInflater.inflate(R.menu.logout_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.miLogout){
+
+            Log.i(TAG, " Logout")
+            auth.signOut()
+            val logOutIntent = Intent(this, MainActivity::class.java)
+            logOutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logOutIntent)
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+
 
 
     private fun setUpViewPager() {
